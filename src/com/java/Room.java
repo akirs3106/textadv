a10a7500ef8b24efbf2dc0d586c1d84d65c013b7
akirs3106 <mc_akirsch24@mcjvs2.org>
2023-09-15 14:03:01 -0400

    Added Logic for Random Enemy Encounters

--- src/com/java/Room.java
+++ src/com/java/Room.java
@@ -2,15 +2,21 @@ package src.com.java;
 
 import java.util.Random;
 public class Room {
-    Enemy[] enemies;
-    Chest chest;
-    String description;
-    String type;
-    int enemiesRemaining;
+    protected Enemy[] enemies;
+    protected Chest chest;
+    protected String description;
+    protected String type;
+    protected int enemiesRemaining;
+    protected boolean hasChests;
 
     public Room(Enemy[] enemies, Chest chest, String description, String type) {
         this.enemies = enemies;
         this.chest = chest;
+        if(this.chest == null) {
+            this.hasChests = false;
+        } else {
+            this.hasChests = true;
+        }
         this.description = description;
         this.type = type;
         this.enemiesRemaining = enemies.length;
@@ -31,6 +37,19 @@ public class Room {
             this.type = roomTypes[0];
         }
 
+    }
+
+    public void playerDecideEncounter(Player plr) {
+        if(this.enemiesRemaining >= 0 && this.type != "boss") {
+            Random random = new Random();
+            int decider = random.nextInt(100) + 1;
+            if(decider <= 25) {
+                Main.startEncounter(Main.createRandomSkeleton(), plr);
+            } else {
+                return;
+            }
+        } 
+        return;
     }
     
 }
